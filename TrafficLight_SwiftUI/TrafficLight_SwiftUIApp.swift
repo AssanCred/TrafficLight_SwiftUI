31b6bd3617f4ea80ca124e148c15735856e1728a
AssanCred <temix@live.ru>
2023-06-09 23:00:39 +0700

    Initial Commit

---
+++ TrafficLight_SwiftUI/TrafficLight_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUIApp.swift
+//  TrafficLight_SwiftUI
+//
+//  Created by Артём Латушкин on 09.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
